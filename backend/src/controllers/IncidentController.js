8b29ca69df7b489572c8b1eae68edb4c82ee1370
Levi Ciarrocchi <levibc44@gmail.com>
2020-03-29 19:17:56 -0300

    Front-end do app

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -43,10 +43,10 @@ const [count] = await connection('incidents').count();
     
     const incident = await connection('incidents')
      .where('id', id)
-     .select('id')
+     .select('ong_id')
      .first();
 
-     if (incident.id != id) {
+     if (incident.ong_id != ong_id) {
          return response.status(401).json({ error: 'Operation not permitted.' });
      }
 
