ab9b3c6dd19331cc612b8ebf282eb5f35264eb61
Levi Ciarrocchi <levibc44@gmail.com>
2020-04-03 01:02:46 -0300

    Mobile da aplicação

---
+++ mobile/App.js
@@ -0,0 +1,13 @@
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR';
+
+import React from 'react';
+
+import Routes from './src/Routes';
+
+export default function App() {
+  return (
+    <Routes />
+  );
+}
+
